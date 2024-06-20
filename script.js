c5acf1e19835f59e09d4acc9515688d21c03e490
Bunjamin <bunjamindullovi2@gmail.com>
2024-06-20 14:05:29 +0200

    bankhotel

---
+++ script.js
@@ -0,0 +1,19 @@
+var burgerMenu = document.getElementById('burger-menu');
+
+var overlay = document.getElementById('menu');
+
+burgerMenu.addEventListener('click', function() {
+  this.classList.toggle("close");
+  overlay.classList.toggle("overlay");
+});
+
+    
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
